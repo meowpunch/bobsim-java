9dd9c9c8591a71f747a22478d817456a80b09781
meowpunch <jinhoonbae@gmail.com>
2020-06-09 16:07:46 +0900

    MEOW: modify migration recipe_items for foregin key

--- src/main/resources/db/migration/V3__create_recipe_items.sql
+++ src/main/resources/db/migration/V3__create_recipe_items.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS recipe_items (
     updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     recipe_id BIGINT NOT NULL,
     item_id BIGINT NOT NULL,
-    UNIQUE recipe_item (recipe_id, item_id)
+    UNIQUE recipe_item (recipe_id, item_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipe(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES item(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
